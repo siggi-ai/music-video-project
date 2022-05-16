87378ffed16df51fc74b62342f52e9d633e2b495
siggi <siegfried.hamm@gmail.com>
2022-05-16 11:14:33 +0200

    spinning spiral

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-       
+       <img src="../../public/images/spirale.jpg" alt="Spirale" class="spirale">
     </div>
 </template>
 <script>
@@ -10,6 +10,17 @@ export default {
 </script>
 <style scoped>
 body {
-    background-image: url("../../public/images/spirale.jpg");
+}
+.spirale {
+    rotate: 60dg;
+    animation: rotate 2s linear infinite;
+}
+@keyframes rotate {
+  0% {
+    transform: rotate(0deg)
+  }
+  100% {
+    transform: rotate(180deg)
+  }
 }
 </style>
